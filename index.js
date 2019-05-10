7b34f9a51dc9a66f80654ea2ff8f47565f381276
momin-ctg <mominriyadh@gmail.com>
2019-05-10 17:32:12 +0600

    file update

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('<h1> Server Working </h1>')
+});
+
+app.listen(3000, ()=>{
+    console.log('Server Working at http://localhost:3000');
+});
